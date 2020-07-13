e466911ea4b6915cb762f676550225c8132bbc0e
Edwin Game Dev <edwinjonesholanda@gmail.com>
2020-07-12 23:49:30 -0300

    Created the game session controller

---
+++ Assets/_Main/Scripts/GameController/GameSessionController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSessionController : MonoBehaviour
+{
+    private GameMasterState MasterState;
+    private GameStateType currentGameStatus = GameStateType.Intro;
+
+    private Dictionary<GameStateType, IGameState> gameStates = new Dictionary<GameStateType, IGameState>();
+
+    public void StartGame()
+    {
+
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        GameLoop();
+    }
+
+    public void ChangeGameState(GameStateType gameState)
+    {
+        currentGameStatus = gameState;
+    }
+
+    private void GameLoop()
+    {
+        // Use the loop of the current game state
+        gameStates[currentGameStatus].Loop();
+    }
+}

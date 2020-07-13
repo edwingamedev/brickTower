2641556c3651537ffae66dbe954f9b26892c9e14
Edwin Game Dev <edwinjonesholanda@gmail.com>
2020-07-12 23:53:49 -0300

    Created a regular input controller

---
+++ Assets/_Main/Scripts/InputController/IInputController.cs
@@ -0,0 +1,4 @@
+﻿public interface IInputController
+{
+    void ProcessInput(InputType input);
+}
---
+++ Assets/_Main/Scripts/InputController/InputController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class InputController : IInputController
+{
+    public GameContainer gameContainer;
+    private GameSessionController gameSessionController;
+
+    public InputController(GameContainer gameContainer, GameSessionController gameSessionController)
+    {
+        this.gameContainer = gameContainer;
+        this.gameSessionController = gameSessionController;
+    }
+
+    private void RotateBlock()
+    {
+        gameContainer.currentPlayingBlock.RotateLeft();
+    }
+
+    private void MoveBlockLeft()
+    {
+        gameContainer.currentPlayingBlock.MoveLeft();
+    }
+
+    private void MoveBlockRight()
+    {
+        gameContainer.currentPlayingBlock.MoveRight();
+    }
+
+    private void MoveBlockDown()
+    {
+        gameContainer.currentPlayingBlock.MoveDown();
+    }
+
+    public void ProcessInput(InputType input)
+    {
+        switch (input)
+        {
+            case InputType.RotateBlock:
+                RotateBlock();
+                break;
+
+            case InputType.MoveBlockLeft:
+                MoveBlockLeft();
+                break;
+
+            case InputType.MoveBlockRight:
+                MoveBlockRight();
+                break;
+
+            case InputType.MoveBlockDown:
+                MoveBlockDown();
+                break;
+
+            default:
+                break;
+        }
+    }
+}

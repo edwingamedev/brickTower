06b12bcb52747d4b15dce6548f62464b46dcfd23
Edwin Game Dev <edwinjonesholanda@gmail.com>
2020-07-12 23:57:10 -0300

    Created the input listener

---
+++ Assets/_Main/Scripts/Events/InputListener.cs
@@ -0,0 +1,24 @@
+﻿using EdwinGameDev.Events;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace EdwinGameDev.InputSystem
+{
+    public class InputListener : MonoBehaviour
+    {
+        public UnityEvent command;
+        public InputEvent inputEvent;
+
+        private void OnEnable()
+        {
+            inputEvent.Assign(command.Invoke);
+        }
+
+        private void OnDisable()
+        {
+            inputEvent.Withhold(command.Invoke);
+        }
+    }
+}

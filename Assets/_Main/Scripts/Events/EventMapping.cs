f8d95791b81bfa216ceacd07399878fbdbca00d4
Edwin Game Dev <edwinjonesholanda@gmail.com>
2020-07-12 23:55:44 -0300

    Added an event mapping

---
+++ Assets/_Main/Scripts/Events/EventMapping.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+using UnityEngine.PlayerLoop;
+using System;
+
+namespace EdwinGameDev.Events
+{
+    [CreateAssetMenu(menuName = "Edwin Game Dev/EventMapping")]
+    public class EventMapping : ScriptableObject
+    {
+        [Serializable]
+        public struct InputCommand
+        {
+            public KeyCode keyCode;
+            public InputEvent command;
+        }
+
+        public List<InputCommand> inputEvents = new List<InputCommand>();
+        private void Update()
+        {
+
+        }
+    }
+}

217bf27c2ab33569b75004c3be3a3075db83ee36
Edwin Game Dev <edwinjonesholanda@gmail.com>
2020-07-12 23:51:19 -0300

    Added intro game state

---
+++ Assets/_Main/Scripts/GameState/States/IntroGameState.cs
@@ -0,0 +1,16 @@
+﻿public class IntroGameState : AGameState
+{
+    public override GameStateType StateType { get ; set; }
+    public override IInputController InputController { get; set; }
+
+    public IntroGameState(IInputController IC)
+    {
+        StateType = GameStateType.Playing;
+        InputController = IC;
+    }
+
+    public override void Loop()
+    {
+        
+    }
+}

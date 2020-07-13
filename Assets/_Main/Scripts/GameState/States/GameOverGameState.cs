62384a2f5c0a4e5477736835ad989ff44af891ee
Edwin Game Dev <edwinjonesholanda@gmail.com>
2020-07-12 23:51:06 -0300

    Added gameover game state

---
+++ Assets/_Main/Scripts/GameState/States/GameOverGameState.cs
@@ -0,0 +1,16 @@
+﻿public class GameOverGameState : AGameState
+{
+    public override GameStateType StateType { get ; set; }
+    public override IInputController InputController { get; set; }
+
+    public GameOverGameState(IInputController IC)
+    {
+        StateType = GameStateType.Playing;
+        InputController = IC;
+    }
+
+    public override void Loop()
+    {
+        
+    }
+}

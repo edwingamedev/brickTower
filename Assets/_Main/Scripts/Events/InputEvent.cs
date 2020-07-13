374b6d81dc02bc7477c515d5e7f8e4b9207a4419
Edwin Game Dev <edwinjonesholanda@gmail.com>
2020-07-12 23:56:31 -0300

    Added an input event system

---
+++ Assets/_Main/Scripts/Events/InputEvent.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace EdwinGameDev.Events
+{
+    public delegate void Execute();  // delegate
+
+    [CreateAssetMenu(menuName = "Edwin Game Dev/InputEvent")]
+    public class InputEvent : ScriptableObject
+    {
+        private Execute execute;
+
+        public void Assign(Execute action)
+        {
+            execute += action;
+        }
+
+        public void Withhold(Execute action)
+        {
+            execute -= action;
+        }
+
+        public void Execute()
+        {
+            execute?.Invoke();
+        }
+
+    }
+}

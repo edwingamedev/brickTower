ce6cf63381e2e05d9e4f65308112761b5f8c6495
Edwin Game Dev <edwinjonesholanda@gmail.com>
2020-07-12 23:52:12 -0300

    Added game master state

---
+++ Assets/_Main/Scripts/GameState/States/GameMasterState.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using UnityEngine;
+
+public class GameMasterState : AGameState
+{
+    public override IInputController InputController { get; set; }
+    public override GameStateType StateType { get; set; }
+
+    private bool gameMasterEnabled = false;
+
+    public GameMasterState(GameMasterInputController inputController)
+    {
+        InputController = inputController;
+        StateType = GameStateType.GameMaster;
+    }
+
+    public override void Loop()
+    {
+        // Enabled or Disable GameMaster
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            gameMasterEnabled = !gameMasterEnabled;
+        }
+
+        if (gameMasterEnabled)
+        {
+            GameMasterCommands();
+        }
+    }
+
+    private void GameMasterCommands()
+    {
+        // Spawn random block
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            
+        }
+
+        // Reset block
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+
+        }
+
+        // Spawn block 01
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SpawnBlock(1);
+        }
+
+        // Spawn block 02
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SpawnBlock(2);
+        }
+
+        // Spawn block 03
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SpawnBlock(3);
+        }
+
+        // Spawn block 04
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SpawnBlock(4);
+        }
+
+        // Spawn block 05
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SpawnBlock(5);
+        }
+
+        // Spawn block 06
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SpawnBlock(6);
+        }
+
+        // Spawn block 07
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SpawnBlock(7);
+        }
+    }
+
+    private void SpawnBlock(int value)
+    {
+
+    }
+}

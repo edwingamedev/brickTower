08d4547e8e9a9150e5592365ae2b2bd46ab04363
Edwin Game Dev <edwinjonesholanda@gmail.com>
2020-07-12 23:51:39 -0300

    Added paused game state

---
+++ Assets/_Main/Scripts/GameState/States/PausedGameState.cs
@@ -0,0 +1,16 @@
+﻿public class PausedGameState : AGameState
+{
+    public override GameStateType StateType { get; set; }
+    public override IInputController InputController { get; set; }
+
+    public PausedGameState(IInputController IC)
+    {
+        StateType = GameStateType.Playing;
+        InputController = IC;
+    }
+
+    public override void Loop()
+    {
+
+    }
+}

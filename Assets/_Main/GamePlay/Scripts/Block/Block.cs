e12399f9445f4f059253b89f3a9ab7cdecb429d0
Edwin Game Dev <edwinjonesholanda@gmail.com>
2020-07-20 03:28:02 -0300

    Created a new block behavior, consisting of moving each piece separately

--- Assets/_Main/GamePlay/Scripts/Block/Block.cs
+++ Assets/_Main/GamePlay/Scripts/Block/Block.cs
@@ -7,86 +7,153 @@ namespace EdwinGameDev
 {
     public class Block : MonoBehaviour
     {
-        private new Transform transform;
-        public new Rigidbody2D rigidbody;
-
-        // Starts with Kinematic type
-        private bool hasPhysics = false;
-
-        private Vector2 leftVector = new Vector2(-0.25f, 0);
-        private Vector2 rightVector = new Vector2(0.25f, 0);
-
-        public ScriptableEvent OnBlockCollided;
-
-        public bool canTurn = true;
-
-        // Falling
-        private Vector2 downVector = new Vector2(0, -0.1f);
-
-        private float fallingRate = 0.07f;
-        private float nextFall = 0f;
+        public BlockType blockType;
+        private Piece[] pieces;
+        private int rotationIndex { get; set; }
+        private Vector2Int[,] offset;
+        public ScriptableEvent onBlockSet;
 
         private void Awake()
         {
-            DisablePhysics();
-            transform = GetComponent<Transform>();
+
         }
 
-        private void FixedUpdate()
+        public void Build(Vector2Int spawnPos, BlockType blockType, Piece[] pieces, Vector2Int[,] offset)
         {
-            MoveDown();
+            transform.name = $"Block {blockType}";
+                        
+            this.blockType = blockType;
+            this.pieces = pieces;
+            this.offset = offset;
         }
 
-        public void RotateLeft()
+        public bool MoveBlock(Vector2Int movement)
         {
-            if (canTurn)
-                transform.Rotate(Vector3.forward, 90);
-        }
-
-        public void MoveLeft()
-        {
-            transform.Translate(leftVector, Space.World);
-        }
-
-        public void MoveRight()
-        {
-            transform.Translate(rightVector, Space.World);
-        }
-
-        public void MoveDown()
-        {
-            if (!hasPhysics && Time.time > nextFall)
+            for (int i = 0; i < pieces.Length; i++)
             {
-                //If the block fall, reset the nextfall time to a new point in the future.
-                nextFall = Time.time + fallingRate;
+                if (!pieces[i].CanTileMove(movement + pieces[i].pieceCoordinates))
+                {
+                    Debug.Log("Cant Go there!");
+                    if (movement == Vector2Int.down)
+                    {
+                        PlacePiece();
+                    }
+                    return false;
+                }
+            }
 
-                transform.Translate(downVector, Space.World);
+            for (int i = 0; i < pieces.Length; i++)
+            {
+                pieces[i].MoveTile(movement);
+            }
+
+            return true;
+        }
+
+        public void Rotate()
+        {
+            RotatePiece(true, true);
+        }
+
+        /// <summary>
+        /// Rotates the piece by 90 degrees in specified direction. Offest operations should almost always be attempted,
+        /// unless you are rotating the piece back to its original position.
+        /// </summary>
+        public void RotatePiece(bool clockwise, bool shouldOffset)
+        {
+            int oldRotationIndex = rotationIndex;
+            rotationIndex += clockwise ? 1 : -1;
+            rotationIndex = Mod(rotationIndex, 4);
+
+            for (int i = 0; i < pieces.Length; i++)
+            {
+                pieces[i].RotateTile(pieces[0].pieceCoordinates, clockwise);
+
+            }
+
+            if (!shouldOffset)
+            {
+                return;
+            }
+
+            bool canOffset = CanOffset(oldRotationIndex, rotationIndex);
+
+            if (!canOffset)
+            {
+                RotatePiece(!clockwise, false);
             }
         }
 
-        public void DisablePhysics()
+        private bool CanOffset(int oldRotationIndex, int rotationIndex)
         {
-            //rigidbody.bodyType = RigidbodyType2D.Kinematic;
-            rigidbody.gravityScale = 0;
-            hasPhysics = false;
-        }
+            Vector2Int offsetVal1, offsetVal2, endOffset;
 
-        public void EnabledPhysics()
-        {
-            rigidbody.bodyType = RigidbodyType2D.Dynamic;
-            rigidbody.gravityScale = 0.2f;
-            hasPhysics = true;
-        }
+            endOffset = Vector2Int.zero;
 
-        private void OnCollisionEnter2D(Collision2D collision)
-        {
-            if (!hasPhysics)
+            bool movePossible = false;
+
+            for (int testIndex = 0; testIndex < 5; testIndex++)
             {
-                EnabledPhysics();
-
-                // Block Collided
-                OnBlockCollided?.Trigger();
+                offsetVal1 = offset[testIndex, oldRotationIndex];
+                offsetVal2 = offset[testIndex, rotationIndex];
+                endOffset = offsetVal1 - offsetVal2;
+                if (CanMovePiece(endOffset))
+                {
+                    movePossible = true;
+                    break;
+                }
             }
+
+            if (movePossible)
+            {
+                MoveBlock(endOffset);
+            }
+            else
+            {
+                Debug.Log("Move impossible");
+            }
+            return movePossible;
+        }
+
+        /// <summary>
+        /// Checks if the piece is able to be moved by the specified amount. A piece cannot be moved if there
+        /// is already another piece there or the piece would end up out of bounds.
+        /// </summary>
+        private bool CanMovePiece(Vector2Int movement)
+        {
+            for (int i = 0; i < pieces.Length; i++)
+            {
+                if (!pieces[i].CanTileMove(movement + pieces[i].pieceCoordinates))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Mod operation that works for positive and negative values.
+        /// </summary>
+        private int Mod(int x, int m)
+        {
+            return (x % m + m) % m;
+        }
+
+        /// <summary>
+        /// Sets the piece in its permanent location.
+        /// </summary>
+        private void PlacePiece()
+        {
+            for (int i = 0; i < pieces.Length; i++)
+            {
+                if (!pieces[i].PlacePiece())
+                {
+                    Debug.Log("GAME OVER!");
+                    return;
+                }
+            }
+
+            onBlockSet.Trigger();
         }
     }
 }

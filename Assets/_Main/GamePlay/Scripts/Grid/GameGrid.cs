6efda099328121b002c81cac935000ca945f0ea6
Edwin Game Dev <edwinjonesholanda@gmail.com>
2020-07-20 03:35:34 -0300

    Created a game grid, that is used to verify the block positions and provide pro, also dealing with game bounds

---
+++ Assets/_Main/GamePlay/Scripts/Grid/GameGrid.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using UnityEngine;
+
+namespace EdwinGameDev
+{
+    [CreateAssetMenu(menuName = "Edwin Game Dev/GameBounds")]
+    public class GameGrid : ScriptableObject
+    {
+        public GameObject gridPiece;
+        public int gridMinX;
+        public int gridMaxX;
+        public int gridMinY;
+        public int gridMaxY;
+
+        private int gridSizeY => Math.Abs(gridMinY) + gridMaxY;
+        private int gridSizeX => Math.Abs(gridMinX) + gridMaxX;
+        private GridUnit[,] fullGrid;
+
+        /// <summary>
+        /// Creates a grid of sized based off of gridSizeX and gridSizeY public variables
+        /// </summary>
+        public void CreateGrid(Transform gridTransform)
+        {
+            fullGrid = new GridUnit[gridSizeX, gridSizeY];
+
+            for (int y = 0; y < gridSizeY; y++)
+            {
+                for (int x = 0; x < gridSizeX; x++)
+                {
+                    GridUnit newGridUnit = new GridUnit(gridPiece, gridTransform, x, y);
+                    fullGrid[x, y] = newGridUnit;
+                }
+            }
+        }
+
+        public bool IsInBounds(Vector2Int coordToTest)
+        {
+            if (coordToTest.x < gridMinX || coordToTest.x >= gridMaxX || coordToTest.y < gridMinY)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public bool IsPosEmpty(Vector2Int coordToTest)
+        {
+            if (coordToTest.y >= gridMaxY)
+            {
+                return true;
+            }
+
+            if (fullGrid[coordToTest.x, coordToTest.y].isOccupied)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        /// <summary>
+        /// Called when a piece is set in place. Sets the grid location to an occupied state.
+        /// </summary>
+        public void OccupyPos(Vector2Int coords, GameObject tileGO)
+        {
+            fullGrid[coords.x, coords.y].isOccupied = true;
+            fullGrid[coords.x, coords.y].tileOnGridUnit = tileGO;
+        }
+    }
+}
---
+++ Assets/_Main/GamePlay/Scripts/Grid/GridUnit.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace EdwinGameDev
+{
+    public class GridUnit
+    {
+        public GameObject gameObject { get; private set; }
+        public GameObject tileOnGridUnit;
+        public Vector2Int location { get; private set; }
+        public bool isOccupied;
+
+        public GridUnit(GameObject newGameObject, Transform boardParent, int x, int y)
+        {
+            gameObject = GameObject.Instantiate(newGameObject, boardParent);
+            location = new Vector2Int(x, y);
+            isOccupied = false;
+
+            gameObject.transform.position = new Vector3(location.x, location.y);
+        }
+    }
+}

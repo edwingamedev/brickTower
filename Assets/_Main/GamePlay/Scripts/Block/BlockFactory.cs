a0862775ae4b6373247605883a5e5ebbcf4ab3be
Edwin Game Dev <edwinjonesholanda@gmail.com>
2020-07-19 22:11:35 -0300

    Created a block factory to get the blocks

---
+++ Assets/_Main/GamePlay/Scripts/Block/BlockFactory.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EdwinGameDev
+{
+    [CreateAssetMenu(menuName = "Edwin Game Dev/BlockContainer")]
+    public class BlockFactory : ScriptableObject
+    {
+        public List<GameObject> blocks;
+
+        public GameObject GetRandomBlock()
+        {
+            return blocks[Random.Range(0, blocks.Count - 1)];
+        }
+    }
+}

43ea2d3c10bf681319eb81a93455e880372437c1
Edwin Game Dev <edwinjonesholanda@gmail.com>
2020-07-19 22:07:20 -0300

    Added some behaviours to the block (Turn,Down,Right,Left)

--- Assets/_Main/GamePlay/Scripts/Block/Block.cs
+++ Assets/_Main/GamePlay/Scripts/Block/Block.cs
@@ -10,42 +10,82 @@ namespace EdwinGameDev
         private new Transform transform;
         public new Rigidbody2D rigidbody;
 
+        // Starts with Kinematic type
         private bool hasPhysics = false;
 
-        private Vector2 leftVector = new Vector2(1, 0);
-        private Vector2 rightVector = new Vector2(1, 0);
+        private Vector2 leftVector = new Vector2(-0.25f, 0);
+        private Vector2 rightVector = new Vector2(0.25f, 0);
+
+        public ScriptableEvent OnBlockCollided;
+
+        public bool canTurn = true;
+
+        // Falling
+        private Vector2 downVector = new Vector2(0, -0.1f);
+
+        private float fallingRate = 0.07f;
+        private float nextFall = 0f;
 
         private void Awake()
         {
+            DisablePhysics();
             transform = GetComponent<Transform>();
+        }
+
+        private void FixedUpdate()
+        {
+            MoveDown();
         }
 
         public void RotateLeft()
         {
-            transform.Rotate(Vector3.forward, 90);
+            if (canTurn)
+                transform.Rotate(Vector3.forward, 90);
         }
 
         public void MoveLeft()
         {
-            transform.Translate(leftVector);
+            transform.Translate(leftVector, Space.World);
         }
 
         public void MoveRight()
         {
-            transform.Translate(rightVector);
+            transform.Translate(rightVector, Space.World);
         }
 
         public void MoveDown()
         {
+            if (!hasPhysics && Time.time > nextFall)
+            {
+                //If the block fall, reset the nextfall time to a new point in the future.
+                nextFall = Time.time + fallingRate;
 
+                transform.Translate(downVector, Space.World);
+            }
+        }
+
+        public void DisablePhysics()
+        {
+            //rigidbody.bodyType = RigidbodyType2D.Kinematic;
+            rigidbody.gravityScale = 0;
+            hasPhysics = false;
+        }
+
+        public void EnabledPhysics()
+        {
+            rigidbody.bodyType = RigidbodyType2D.Dynamic;
+            rigidbody.gravityScale = 0.2f;
+            hasPhysics = true;
         }
 
         private void OnCollisionEnter2D(Collision2D collision)
         {
             if (!hasPhysics)
             {
-                rigidbody.bodyType = RigidbodyType2D.Dynamic;
-                hasPhysics = true;
+                EnabledPhysics();
+
+                // Block Collided
+                OnBlockCollided?.Trigger();
             }
         }
     }

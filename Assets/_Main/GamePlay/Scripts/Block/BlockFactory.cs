5f6471a2acf08732c5d9c55e37029cf5a70c22de
Edwin Game Dev <edwinjonesholanda@gmail.com>
2020-07-20 03:30:31 -0300

    Created a block factory, to handle the creation and spawning new blocks

--- Assets/_Main/GamePlay/Scripts/Block/BlockFactory.cs
+++ Assets/_Main/GamePlay/Scripts/Block/BlockFactory.cs
@@ -7,11 +7,31 @@ namespace EdwinGameDev
     [CreateAssetMenu(menuName = "Edwin Game Dev/BlockContainer")]
     public class BlockFactory : ScriptableObject
     {
-        public List<GameObject> blocks;
+        public GameSettings gameSettings;
+        public GameObject blockPrefab;
 
-        public GameObject GetRandomBlock()
+        public Block SpawnBlock(BlockType blockType, Transform spawnPoint)
         {
-            return blocks[Random.Range(0, blocks.Count - 1)];
+            GameObject blockGO = Instantiate(blockPrefab, spawnPoint.position, Quaternion.identity, spawnPoint);
+            Block block = blockGO.GetComponent<Block>();
+
+            Vector2Int spawnPosition = new Vector2Int((int)spawnPoint.position.x, (int)spawnPoint.position.y);
+            Piece[] blockPieces = GetBlockPieces(spawnPosition, blockType, blockGO.transform);
+
+            block.Build(spawnPosition, blockType, blockPieces, gameSettings.GetOffSet(blockType));
+
+            return block;
+        }
+
+        public Block SpawnRandomBlock(Transform spawnPoint)
+        {
+            int randomIndex = Random.Range(0, BlockType.GetNames(typeof(BlockType)).Length);
+            return SpawnBlock((BlockType)randomIndex, spawnPoint);
+        }
+
+        private Piece[] GetBlockPieces(Vector2Int spawnPos, BlockType blockType, Transform parent)
+        {
+            return gameSettings.GetBlockPieces(spawnPos, blockType, parent);
         }
     }
 }

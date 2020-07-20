6c802f9f21bcbd4d9680528651b35cf152cd9620
Edwin Game Dev <edwinjonesholanda@gmail.com>
2020-07-20 03:31:34 -0300

    Created a block controller that handles the controlling of current used block

---
+++ Assets/_Main/GamePlay/Scripts/Block/BlockController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace EdwinGameDev
+{
+    [Serializable]
+    public struct BlockCommands
+    {
+        public ScriptableEvent moveLeft;
+        public ScriptableEvent moveRight;
+        public ScriptableEvent moveDown;
+        public ScriptableEvent turnBlock;
+    }
+
+    public class BlockController : MonoBehaviour
+    {
+        public GameContainer gameContainer;
+        public BlockCommands blockCommands;
+        private float currentDropTick;
+        private void OnEnable()
+        {
+            blockCommands.moveLeft.OnTriggered += BlockMoveLeft;
+            blockCommands.moveRight.OnTriggered += BlockMoveRight;
+            blockCommands.moveDown.OnTriggered += BlockMoveDown;
+            blockCommands.turnBlock.OnTriggered += BlockTurn;
+
+            gameContainer.blocksOfSession.Clear();
+        }
+
+        private void OnDisable()
+        {
+            blockCommands.moveLeft.OnTriggered -= BlockMoveLeft;
+            blockCommands.moveRight.OnTriggered -= BlockMoveRight;
+            blockCommands.moveDown.OnTriggered -= BlockMoveDown;
+            blockCommands.turnBlock.OnTriggered -= BlockTurn;
+        }
+
+        private void Update()
+        {
+            DropBlock();
+        }
+
+        private void DropBlock()
+        {
+            if (Time.time > currentDropTick)
+            {
+                BlockMoveDown();
+                currentDropTick = Time.time + gameContainer.blockDropRate;
+            }
+        }
+
+        public void BlockMoveLeft()
+        {
+            gameContainer?.MoveBlock(Vector2Int.left);
+        }
+
+        public void BlockMoveRight()
+        {
+            gameContainer?.MoveBlock(Vector2Int.right);
+        }
+
+        public void BlockMoveDown()
+        {
+            gameContainer?.MoveBlock(Vector2Int.down);
+        }
+
+        public void BlockTurn()
+        {
+            gameContainer?.RotateBlock();
+        }
+    }
+}

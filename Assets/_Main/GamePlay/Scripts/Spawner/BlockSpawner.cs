76b8fbe9928364afae5f9bf907600d7055999a85
Edwin Game Dev <edwinjonesholanda@gmail.com>
2020-07-19 22:12:23 -0300

    Created the block spawner and added the behaviour to spawn a random block

---
+++ Assets/_Main/GamePlay/Scripts/Spawner/BlockSpawner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EdwinGameDev
+{
+    public class BlockSpawner : MonoBehaviour
+    {
+        public Transform spawnPoint;
+        public BlockFactory blockFactory;
+        public GameContainer gameContainer;
+
+        public void SpawnRandomBlock()
+        {
+            GameObject blockGO = Instantiate(blockFactory.GetRandomBlock(), spawnPoint.position, Quaternion.identity, spawnPoint);
+            Block block = blockGO.GetComponent<Block>();
+            gameContainer.blocksOfSession.Add(block);
+        }
+    }
+}

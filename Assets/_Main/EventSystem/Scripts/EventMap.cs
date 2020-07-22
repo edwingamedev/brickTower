3050e446d581bc0187839af8d3c3cac09a6f2000
Edwin Game Dev <edwinjonesholanda@gmail.com>
2020-07-22 06:49:55 -0300

    Created event map system, to send events using scriptable object

---
+++ Assets/_Main/EventSystem/Scripts/EventMap.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EdwinGameDev
+{
+    [CreateAssetMenu(menuName = "Edwin Game Dev/EventMap")]
+    public class EventMap : ScriptableObject
+    {
+        public List<EventMapper> eventMappers;
+    }
+}
---
+++ Assets/_Main/EventSystem/Scripts/EventMapper.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace EdwinGameDev
+{
+    [Serializable]
+    public struct EventMapper
+    {
+        public string eventName;
+        public ScriptableEvent scriptableEvent;
+    }
+}

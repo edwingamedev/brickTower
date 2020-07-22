3c4c54eb29c9038dd9d13059d9726c1d8a27effa
Edwin Game Dev <edwinjonesholanda@gmail.com>
2020-07-22 06:50:40 -0300

    Added moving clouds to the game

---
+++ Assets/_Main/CloudSystem/Cloud.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EdwinGameDev
+{
+    public class Cloud : MonoBehaviour
+    {
+        private float currentMoveTick;
+        public GameContainer gameContainer;
+        public GameGrid gameGrid;
+        public Vector2 direction;
+        public SpriteRenderer spriteRenderer;
+        bool canmove = false;
+
+        private void Update()
+        {
+            Move();
+        }
+
+        public void SetDirection(bool moveRight)
+        {
+            direction = moveRight ? Vector2.right / 5 : Vector2.left / 5;
+        }
+
+        public void InitCloud(Sprite sprite, Color color, bool moveRight)
+        {
+            SetColor(color);
+            SetSprite(sprite);
+            SetDirection(moveRight);
+
+            canmove = true;
+        }
+
+        public void SetColor(Color color)
+        {
+            spriteRenderer.color = color;
+        }
+
+        public void SetSprite(Sprite sprite)
+        {
+            spriteRenderer.sprite = sprite;
+        }
+
+
+        private void Move()
+        {
+            if (!canmove)
+                return;
+
+            if (Time.time > currentMoveTick)
+            {
+                gameObject.transform.position = gameObject.transform.position + new Vector3(direction.x, direction.y);
+                currentMoveTick = Time.time + .25f;
+
+                // Left
+                if (gameObject.transform.position.x < gameGrid.gridMinX * 2)
+                {
+                    Destroy(gameObject);
+                }
+
+                // Right
+                if (gameObject.transform.position.x > gameGrid.gridMaxX * 2)
+                {
+                    Destroy(gameObject);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/_Main/CloudSystem/CloudSpawner.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EdwinGameDev
+{
+    public class CloudSpawner : MonoBehaviour
+    {
+        public GameGrid gameGrid;
+        public Sprite[] cloudSprites;
+        public Color[] colors;
+        private float currentMoveTick;
+        public GameObject cloudPrefab;
+        public List<GameObject> clouds;
+
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Time.time > currentMoveTick)
+            {
+                SpawnCloud();
+                currentMoveTick = Time.time + Random.Range(2,5);
+            }
+        }
+
+        public void SpawnCloud()
+        {
+            bool dir = Random.Range(0, 2) != 0;
+
+            Vector3 spawnPos = dir ? new Vector3(gameGrid.gridMinX * 1.5f, Random.Range(gameGrid.gridMinY, gameGrid.gridMaxY)) : 
+                                    new Vector3(gameGrid.gridMaxX * 1.5f, Random.Range(gameGrid.gridMinY, gameGrid.gridMaxY));
+
+            GameObject cloudGO = Instantiate(cloudPrefab, spawnPos, Quaternion.identity);
+            Cloud cloud = cloudGO.GetComponent<Cloud>();
+
+            int randomSprite = Random.Range(0, cloudSprites.Length);
+            int randomColor = Random.Range(0, colors.Length);
+
+            cloud.InitCloud(cloudSprites[randomSprite], colors[randomColor], dir);
+
+            clouds.Add(cloudGO);
+        }
+    }
+}

e418d257bd5f96af9d938493ad9f2f76bdcaed45
Edwin Game Dev <edwinjonesholanda@gmail.com>
2020-07-22 06:52:09 -0300

    Created string typed scriptable event + custom class for sending the events using event mappers

---
+++ Assets/_Main/EventSystem/Scripts/StringScriptableEvent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+namespace EdwinGameDev
+{
+    [CreateAssetMenu(menuName = "Edwin Game Dev/String ScriptableEvent")]
+    public class StringScriptableEvent : ScriptableObject
+    {
+        public event Action<string> OnTriggered;
+        public void Trigger(string args)
+        {
+            OnTriggered?.Invoke(args);
+        }
+    }
+}

108f996f8ce9efaa13a87355b0709a3985b34fa3
Edwin Game Dev <edwinjonesholanda@gmail.com>
2020-07-22 06:46:56 -0300

    Created game screens to handle screen inputs using events

---
+++ Assets/_Main/GamePlay/Scripts/GameController/GameScreen.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+namespace EdwinGameDev
+{
+    [Serializable]
+    public struct GameScreenSettings
+    {
+        public GameStateType gameStateType;
+        public GameObject screenPrefab;
+        public EventMap eventMap;
+    }
+}
---
+++ Assets/_Main/GamePlay/Scripts/GameController/GameScreens.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace EdwinGameDev
+{
+    [CreateAssetMenu(menuName = "Edwin Game Dev/GameScreens")]
+    public class GameScreens : ScriptableObject
+    {        
+        public List<GameScreenSettings> screens = new List<GameScreenSettings>();        
+    }
+}

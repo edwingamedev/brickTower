78833a5568748bf8bdb6f098529cb490823de721
Edwin Game Dev <edwinjonesholanda@gmail.com>
2020-07-22 07:09:17 -0300

    Created a class that keeps track of game score and can send event when no move lives

---
+++ Assets/_Main/GamePlay/Scripts/Score/GameScore.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EdwinGameDev
+{
+    public class GameScore : MonoBehaviour
+    {
+        public int lives;
+        private int currentLives;
+        public ScriptableEvent noMoveLives;
+
+        public void ResetLives()
+        {
+            currentLives = lives;
+        }
+
+        public void AddLife()
+        {
+            currentLives++;
+        }
+
+        public void RemoveLife()
+        {
+            currentLives--;
+
+            if (currentLives <= 0)
+                NoMoreLives();
+        }
+
+        public void NoMoreLives()
+        {
+            noMoveLives.Trigger();
+        }
+
+    }
+}

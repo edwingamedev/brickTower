bdb23ca1e866389ab028087b9b21a7636ebfe6c7
Edwin Game Dev <edwinjonesholanda@gmail.com>
2020-07-22 07:05:41 -0300

    Game session now can control the game states and send events to the current state

--- Assets/_Main/GamePlay/Scripts/GameController/GameSessionController.cs
+++ Assets/_Main/GamePlay/Scripts/GameController/GameSessionController.cs
@@ -1,55 +1,81 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 namespace EdwinGameDev
 {
     public class GameSessionController : MonoBehaviour
     {
         private GameStateType currentGameStatus = GameStateType.Intro;
+        private GameStateType previousGameStatus = GameStateType.Intro;
 
         [SerializeField]
         private Dictionary<GameStateType, IGameState> gameStates = new Dictionary<GameStateType, IGameState>();
-
-        public GameGrid gameGrid;
-        public Transform grid;
-
-        public void StartGame()
-        {
-            gameStates.Clear();
-
-            GameStateFactory gameStateFactory = new GameStateFactory();
-
-            List<IGameState> LGS = gameStateFactory.MakeAllStates();
-
-            foreach (IGameState item in LGS)
-            {
-                gameStates.Add(item.StateType, item);
-            }
-
-            gameGrid.CreateGrid(grid);
-        }
+        public GameScreens gameScreens;
+        public StringScriptableEvent gameSessionEvent;
 
         private void Start()
         {
-            StartGame();
+            gameSessionEvent.OnTriggered += Execute;
+            Init();
         }
 
-        private void Update()
+        public void Init()
         {
-           // GameLoop();
+            gameStates.Clear();
+
+            // Fill Game states
+            GameStateFactory gameStateFactory = new GameStateFactory();
+
+            foreach ((GameScreenSettings settings, IGameState gamestate) in from GameScreenSettings screen in gameScreens.screens
+                                                                            let gamestate = gameStateFactory.MakeGameState(screen)
+                                                                            select (screen, gamestate))
+            {
+                gameStates.Add(settings.gameStateType, gamestate);
+            }
+
+            // Sets to intro game state
+            ChangeGameState(GameStateType.Intro);
+            Execute("init");
+        }
+
+        public void Execute(string eventName)
+        {
+            Debug.Log($"{currentGameStatus} Screen execute: {eventName}");
+
+            // Use the loop of the current game state
+            gameStates[currentGameStatus].Execute(eventName.ToLower());
+        }
+
+        public void StartGame(string eventName)
+        {
+            ChangeGameState(GameStateType.Playing);
+            Execute(eventName);
+        }
+
+        public void PauseGame(string eventName)
+        {
+            ChangeGameState(GameStateType.Paused);
+            Execute(eventName);
+        }
+
+        public void GameOver(string eventName)
+        {
+            ChangeGameState(GameStateType.GameOver);
+            Execute(eventName);
         }
 
         public void ChangeGameState(GameStateType gameState)
         {
+            previousGameStatus = currentGameStatus;
             currentGameStatus = gameState;
         }
 
-        private void GameLoop()
+        private void OnDestroy()
         {
-            // Use the loop of the current game state
-            gameStates[currentGameStatus].Loop();
+            gameSessionEvent.OnTriggered -= Execute;
         }
+
     }
 }

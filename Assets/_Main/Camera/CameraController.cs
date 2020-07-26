1333c63c1fd759f46da0a2f79263543c609e11eb
Edwin Game Dev <edwinjonesholanda@gmail.com>
2020-07-26 01:18:50 -0300

    Adjusted Camera and spawn point to different screen resolutions

--- Assets/_Main/Camera/CameraController.cs
+++ Assets/_Main/Camera/CameraController.cs
@@ -10,14 +10,30 @@ namespace EdwinGameDev
         public Transform spawnPoint;
         public Transform target;
         public float smoothTime = 1F;
+        public Vector3 cameraOffSet = new Vector3(5, 6.5f, -10);
+        public float cameraSize;
+
+        private Camera camera;
         private Vector3 velocity = Vector3.zero;
         private Vector3 startingTargetPos;
         private Vector3 startingCameraPos;
 
+
+        private void Awake()
+        {
+            camera = GetComponent<Camera>();
+            float screenRatio = (float)Screen.height / (float)Screen.width;
+            camera.orthographicSize = cameraSize * screenRatio * .5f;
+        }
+
         private void Start()
         {
             startingTargetPos = target.position;
+            transform.position = target.TransformPoint(new Vector3(cameraOffSet.x, camera.orthographicSize - cameraOffSet.y, cameraOffSet.z));
             startingCameraPos = transform.position;
+
+            // SPAWN
+            spawnPoint.position = new Vector3(spawnPoint.position.x, camera.orthographicSize + transform.position.y - 1.5f);
         }
 
         public void ResetCameraPos()
@@ -37,7 +53,7 @@ namespace EdwinGameDev
         void Update()
         {
             // Define a target position above and behind the target transform
-            Vector3 targetPosition = target.TransformPoint(new Vector3(5, 9.5f, -10));
+            Vector3 targetPosition = target.TransformPoint(new Vector3(cameraOffSet.x, camera.orthographicSize - cameraOffSet.y, cameraOffSet.z));
 
             // Smoothly move the camera towards that target position
             transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
